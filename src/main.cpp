37e31f4988956a7ac7e4bb2c23c514382aa22f7c
Juan Rey <email@juanrey.net>
2025-11-17 18:48:47 +0100

    Refactor main.cpp to remove COM and common controls

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,17 @@
 #include "MainWindow.h"
 #include <windows.h>
-#include <iostream>
 
 int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow )
 {
   UNREFERENCED_PARAMETER( hPrevInstance );
   UNREFERENCED_PARAMETER( pCmdLine );
-  // Inicializar COM
+
   HRESULT hr = CoInitialize( nullptr );
   if( FAILED( hr ) )
   {
-    std::cerr << "Error al inicializar COM" << std::endl;
     return 1;
   }
-
-  INITCOMMONCONTROLSEX icex;
-  icex.dwSize = sizeof( INITCOMMONCONTROLSEX );
-  icex.dwICC = ICC_WIN95_CLASSES;
-  InitCommonControlsEx( &icex );
-
+  
   MainWindow mainWindow;
 
   if( !mainWindow.Create( hInstance, nCmdShow ) )
@@ -28,7 +21,6 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLin
 
   mainWindow.RunMessageLoop();
 
-  // Finalizar COM
   CoUninitialize();
   return 0;
 }
