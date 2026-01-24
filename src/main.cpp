10e8553628263bc7e29a8db5bd2858356e170776
Juan Rey <email@juanrey.net>
2026-01-24 14:27:18 +0100

    Enforce single-instance app with mutex and window focus
    Implemented a named mutex to ensure only one instance of the application runs at a time. If a second instance is started, it brings the existing window to the foreground and restores it if minimized, instead of launching a new window. Properly handles resource cleanup for both mutex and COM initialization.

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,46 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLin
   UNREFERENCED_PARAMETER( hPrevInstance );
   UNREFERENCED_PARAMETER( pCmdLine );
 
-  HRESULT hr = CoInitialize( nullptr );
-  if( FAILED( hr ) )
+  HANDLE hInstanceMutex = CreateMutex( nullptr, TRUE, L"SimonSaysSingletonMutex" );
+  if( hInstanceMutex == nullptr )
   {
     return 1;
   }
+
+  if( GetLastError() == ERROR_ALREADY_EXISTS )
+  {
+    HWND hExisting = FindWindow( CLASS_NAME, nullptr );
+    if( hExisting )
+    {
+      if( !IsWindowVisible( hExisting ) )
+      {
+        ShowWindow( hExisting, SW_SHOW );
+      }
+      ShowWindow( hExisting, SW_RESTORE );
+      SetForegroundWindow( hExisting );
+      SetWindowPos( hExisting, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE );
+    }
+  }
   else
   {
-    MainWindow mainWindow;
-
-    if( !mainWindow.Create( hInstance, nCmdShow ) )
+    HRESULT hr = CoInitialize( nullptr );
+    if( FAILED( hr ) )
     {
-      return 0;
+      CloseHandle( hInstanceMutex );
+      return 1;
     }
+    else
+    {
+      MainWindow mainWindow;
 
-    mainWindow.RunMessageLoop();
+      if( mainWindow.Create( hInstance, nCmdShow ) )
+      {
+        mainWindow.RunMessageLoop();
+      }
+    }
+    CoUninitialize();
   }
 
-  CoUninitialize();
+  CloseHandle( hInstanceMutex );
   return 0;
 }
